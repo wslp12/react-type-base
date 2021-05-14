1a21a943ae2c8232bbf52f78e965373c822a5b59
윤지만 <yunjiman1992@douzone.com>
2021-05-14 15:39:04 +0900

    prettier && allowHostHeader 예제 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,45 @@
-var path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  target: "es5",
-  entry: path.resolve(__dirname, "src/index.tsx"),
+  mode: 'development',
+  target: 'es5',
+  entry: path.resolve(__dirname, 'src/index.tsx'),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: [".js", ".ts", ".tsx"],
+    extensions: ['.js', '.ts', '.tsx'],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
+    // external 0.0.0.0
+    host: 'localhost',
+    allowedHosts: ['.lululala.co.kr'],
     port: 9000,
   },
   module: {
     rules: [
       {
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
         test: /\.(ts|tsx)/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        use: 'ts-loader',
       },
       {
         include: [
-          path.resolve(__dirname, "src"),
-          path.resolve(__dirname, "playground"),
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'playground'),
         ],
         test: /\.css$/,
         exclude: /node_modules/,
-        use: "css-loader",
+        use: 'css-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
+      template: path.resolve(__dirname, 'public/index.html'),
     }),
   ],
 };
