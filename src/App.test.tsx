83d5d6ef74cd8fb81fe025a3d8bb645076c607b6
윤지만 <yunjiman1992@douzone.com>
2021-09-27 11:36:52 +0900

    .

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,7 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
-
-const user = {
-  id: 202,
-};
 
 describe('App', () => {
   it('App 컴포넌트가 hello 가 출력이 됨 ', () => {
