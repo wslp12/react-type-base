9d7dadfc462cc98f88d4f4bc7a5e42594ce24008
jimmy <jimmy@collabee.co>
2022-04-20 14:41:50 +0900

    react 버전 변경에 따른 구문 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Hello from './components/Hello';
-// import App from '../dist-type/App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/main" element={<Hello />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const id = 'root';
+const rootElem = document.getElementById(id);
+if (rootElem) {
+  const root = ReactDOM.createRoot(rootElem);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="/main" element={<Hello />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+} else {
+  console.error(
+    `${rootElem} 가 존재하지 않습니다. (${id} 가 존재하지 않을 수도 있습니다)`,
+  );
+}
