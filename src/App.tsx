6a3d3591dcf5bae4c6f52f3b3a180c3998c230a2
jimmy <jimmy@collabee.co>
2022-08-25 14:02:01 +0900

    라우팅 예제추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
 import { Link, Outlet } from 'react-router-dom';
-// import ha from './assets/ha.jpeg';
+import ha from './assets/ha.jpeg';
 
 function App() {
   return (
     <>
-      {/* <img src={ha} alt="화평씨 얼굴" style={{ width: 300 }} /> */}
+      <img src={ha} alt="화평씨 얼굴" style={{ width: 300 }} />
       <div data-testid="app-1">
         {process.env.ASDF}
-        Hello
+        test Hello
       </div>
       <nav>
-        <Link to="main">main</Link>
+        {/* <Link to="main">main</Link> */}
+        <Link to="main/1">main/1</Link>
+        <br />
+        <Link to="main/1/item/2">main/1/item/2</Link>
+        <br />
+        <Link to="main/1/item/3">main/1/item/3</Link>
         <br />
         <Link to="/">Dashboard</Link>
       </nav>
