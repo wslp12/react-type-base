df5bc6417095b64e64ca6d05bff953c8707e9746
jimmy <jimmy@collabee.co>
2022-08-28 00:29:13 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Link, Outlet } from 'react-router-dom';
-import ha from './assets/ha.jpeg';
+// import ha from './assets/ha.jpeg';
 
 function App() {
   return (
     <>
-      <img src={ha} alt="화평씨 얼굴" style={{ width: 300 }} />
+      {/* <img src={ha} alt="화평씨 얼굴" style={{ width: 300 }} /> */}
       <div data-testid="app-1">
         {process.env.ASDF}
         test Hello
